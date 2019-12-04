8d6a390dd445e1912a141e4e934b1a1c106427e3
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2019-12-04 11:29:36 -0800

    include example of webhook

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
-'use strict';
-
-const fs = require("fs");
 const unleash = require('unleash-server');
 const axios = require('axios');
 const basicAuthentication = require('./basic-auth-hook');
 
-// demo code for enabling webhooks
-/*
 function onEventHook(event, eventData) {
   const { createdBy: user, data } = eventData;
   let text = '';
 
-  const unleashUrl = 'http://unleash-server-service.unleash.YOURIP.xip.io/';
+  const unleashUrl = 'http://YOUR_UNLEASH_IP';
   const feature = `<${unleashUrl}/#/features/strategies/${data.name}|${
     data.name
   }>`;
 
   switch (event) {
-    case 'feature-created':
+    //case 'feature-created':
     case 'feature-updated': {
-      const verb =
-        event === 'feature-created' ? 'created a new' : 'updated the';
-      text = `${user} ${verb} feature ${feature}\ndescription: ${
-        data.description
-      }\nenabled: ${data.enabled}\nstrategies: \`${JSON.stringify(
-        data.strategies,
-      )}\``;
+
+      payload = {
+        "eventType": "CUSTOM_CONFIGURATION",
+        "attachRules": {
+          "tagRule": [
+            {
+              "meTypes": [
+                "SERVICE"
+              ],
+              "tags": [
+                {
+                  "context": "CONTEXTLESS",
+                  "key": "service",
+                  "value": `${data.description}`
+                },
+              ]
+            }
+          ]
+        },
+        "source": "Unleash via Keptn",
+        "description": `Feature toggle ${data.name} set to value: ${data.enabled}`,
+        "configuration": `${data.name}`,
+        "customProperties": {
+          "UnleashServer": `${unleashUrl}/#/features/strategies/${data.name}`
+        }
+      };
       break;
     }
-    case 'feature-archived':
-    case 'feature-revived': {
-      const verb = event === 'feature-archived' ? 'archived' : 'revived';
-      text = `${user} ${verb} the feature ${feature}`;
-      break;
-    }
+    // case 'feature-archived':
+    // case 'feature-revived': {
+    //   const verb = event === 'feature-archived' ? 'archived' : 'revived';
+    //   text = `${user} ${verb} the feature ${feature}`;
+    //   break;
+    // }
     default: {
       console.error(`Unknown event ${event}`);
       return;
@@ -42,12 +56,9 @@ function onEventHook(event, eventData) {
 
   axios
     .post(
-      'https://webhook.site/YOURID',
-      {
-        username: 'Unleash',
-        icon_emoji: ':unleash:', // if you added a custom emoji, otherwise you can remove this field.
-        text: text,
-      },
+      'https://DT_TENANT/api/v1/events',
+      payload,
+      { headers : { 'Authorization': 'Api-Token DT_API_TOKEN', 'Content-Type': 'application/json'} }
     )
     .then(res => {
       console.log(`Webhook post statusCode: ${res.status}. Text: ${text}`);
@@ -63,13 +74,6 @@ const options = {
   secret: 'mysecret',
   preRouterHook: basicAuthentication,
 };
-*/
-
-const options = {
-    adminAuthentication: 'custom',
-    secret: 'mysecret',
-    preRouterHook: basicAuthentication,
-  };
 
 unleash.start(options).then(server => {
   console.log(`Unleash started on http://localhost:${server.app.get('port')}`);
